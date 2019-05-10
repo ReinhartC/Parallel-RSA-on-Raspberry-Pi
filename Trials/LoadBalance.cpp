114c531dd162948abe6f1425838d5fff6179643c
Reinhart Caesar <sgs3.rc@gmail.com>
2019-05-11 02:38:02 +0700

    Update LoadBalance.cpp
    Optimization

--- Trials/LoadBalance.cpp
+++ Trials/LoadBalance.cpp
@@ -6,41 +6,40 @@ using namespace std;
 int main(){
 	float time[3];
 	float work[3];
+	float sum;
 	int load[3];
-	float line=3291;
+	float line=100;
 	
-	time[0]=12;
-	time[1]=48;
-	time[2]=240;
+	time[0]=167;
+	time[1]=81;
+	time[2]=483;
+//
+   for(int i=0; i<3;i++){
+      work[i] = 1/time[i];
+      sum += work[i];
+   }	
 	
-	for(int i=0; i<3;i++) 
-		work[i]= 1/time[i];
+   for(int i=0; i<3;i++) 
+      load[i] = (work[i]/sum)*line;
 	
-	float sum = work[0]+work[1]+work[2];
-	float remain = 1-sum;
-	
-	for(int i=0; i<3;i++) 
-		work[i]+=remain*work[i]/sum;
-	
-	for(int i=0; i<3;i++) 
-		load[i]=work[i]*line;
-		
-	int node=0;
-	while(load[0]+load[1]+load[2]!=line){
-		if(load[0]+load[1]+load[2]<line) 
-			load[node]++;
-		else if(load[0]+load[1]+load[2]>line) 
-			load[node]--;
-		
-		if(node<3) node++;
-		else node=0;
-	}
-	
+   int node=0;
+   int total = load[0]+load[1]+load[2];
+
+   while(total!=line){
+      if(total<line) load[node]++;
+      else if(total>line) load[node]--;
+
+      total = load[0]+load[1]+load[2];
+
+      if(node<3) node++;
+      else node=0;
+   }
+//	
 	cout << endl << "Adjusted load balance" << endl;
 	cout<< load[0] << endl;
 	cout<< load[1] << endl;
 	cout<< load[2] << endl; 
-	cout << load[0]+load[1]+load[2]<<endl;
+	cout << load[0]+load[1]+load[2] << endl;
 	cout << "Actual line: "<< line << endl;
 	
 	return 0;
